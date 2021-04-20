92257c61874d3e8af86232ba6f101e612b5b6535
Kais <kais.lamine@esprit.tn>
2021-04-20 05:10:30 +0100

    back events integrated

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -135,5 +135,123 @@ class EvenementController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/back/index", name="evenement_back_index", methods={"GET"})
+     */
+    public function indexback(): Response
+    {
+        $evenements = $this->getDoctrine()
+            ->getRepository(Evenement::class)
+            ->findAll();
+        $categories = $this->getDoctrine()
+            ->getRepository(Categorie::class)
+            ->findAll();
+
+        return $this->render('evenement/backindex.html.twig', [
+            'evenements' => $evenements,'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/back/new", name="evenement_back_new", methods={"GET","POST"})
+     */
+    public function newback(Request $request): Response
+    {
+        $evenement = new Evenement();
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+//            var_dump($evenement);
+//            die();
+            $evenement->upload();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($evenement);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('evenement_back_index');
+        }
+
+        return $this->render('evenement/backnew.html.twig', [
+            'evenement' => $evenement,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/back/{id}", name="evenement_back_show", methods={"GET"})
+     */
+    public function showback(Evenement $evenement): Response
+    {
+        return $this->render('evenement/backshow.html.twig', [
+            'evenement' => $evenement,
+        ]);
+    }
+
+    /**
+     * @Route("/back/{id}/edit", name="evenement_back_edit", methods={"GET","POST"})
+     */
+    public function editback(Request $request, Evenement $evenement): Response
+    {
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('evenement_back_index');
+        }
+
+        return $this->render('evenement/backedit.html.twig', [
+            'evenement' => $evenement,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/back/{id}", name="evenement_back_delete", methods={"POST"})
+     */
+    public function deleteback(Request $request, Evenement $evenement): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$evenement->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($evenement);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('evenement_back_index');
+    }
+
+
+
+
+    /**
+     * @Route("/back/", name="evenement_back_search", methods={"POST"})
+     */
+    public function searchback(EvenementRepository $evenementRepo, Request $request)
+    {
+        $data=$request->get('mots');
+        $evenements = $evenementRepo->search($data);
+        $categories = $this->getDoctrine()
+            ->getRepository(Categorie::class)
+            ->findAll();
+
+        return $this->render('evenement/backindex.html.twig', [
+            'evenements' => $evenements, 'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/back/{name}", name="evenement_back_cat", methods={"GET"})
+     */
+    public function FindByCategorieback(EvenementRepository $evenementRepo, Categorie $categorie): Response
+    {
+
+        $evenements = $evenementRepo->FindByCategorie($categorie);
+
+        return $this->render('evenement/backindex.html.twig', [
+            'evenements' => $evenements, 'categories' => $categories,
+        ]);
+    }
 
 }
