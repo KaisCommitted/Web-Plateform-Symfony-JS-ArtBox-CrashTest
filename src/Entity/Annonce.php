605c62597e39ff47c51bafd784dbcd0c2affabe7
Yasmine <yasmine.zerai@esprit.tn>
2021-04-22 12:00:38 +0100

    working yasmine 2
    crud+saisie+captcha+qr+front

--- src/Entity/Annonce.php
+++ src/Entity/Annonce.php
@@ -6,7 +6,10 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
 /**
  * Annonce
