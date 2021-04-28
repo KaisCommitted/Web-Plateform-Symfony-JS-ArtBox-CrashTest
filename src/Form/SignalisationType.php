3d3c19c3b369c4652d52852f2ca4b26d7d0f76ee
Mojitoooo <47983778+Mojitoooo@users.noreply.github.com>
2021-04-28 21:11:28 +0100

    signalcaptcha

--- src/Form/SignalisationType.php
+++ src/Form/SignalisationType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Signalisation;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,6 +19,7 @@ class SignalisationType extends AbstractType
             ->add('dateSignal')
             ->add('idPost')
             ->add('idUser')
+            ->add('captcha', CaptchaType::class,array('attr' => array('class' => 'form-control'), 'label' => 'Enter Captcha Code  '));
         ;
     }
 
