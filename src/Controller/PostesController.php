ef7819c8677301d9c34b29d2412f276b8768e404
Adimossk <78953335+Adimossk@users.noreply.github.com>
2021-04-29 01:38:31 +0100

    ye rabi

--- src/Controller/PostesController.php
+++ src/Controller/PostesController.php
@@ -241,4 +241,231 @@ class PostesController extends AbstractController
     }
 
 
+
+
+
+
+
+
+    /**
+     * @Route("/back/index", name="postes_back_index", methods={"GET"})
+     */
+    public function indexback(): Response
+    {
+        $postes = $this->getDoctrine()
+            ->getRepository(Postes::class)
+            ->findAll();
+
+        $categories = $this->getDoctrine()
+            ->getRepository(Categorie::class)
+            ->findAll();
+
+        return $this->render('postes/backindex.html.twig', [
+            'postes' => $postes,'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/back/new", name="postes_back_new", methods={"GET","POST"})
+     */
+    public function newback(Request $request): Response
+    {
+        $poste = new Postes();
+
+        $poste->setPostDate($poste->getPostDate());
+        $form = $this->createForm(PostesType::class, $poste);
+        $form->handleRequest($request);
+
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $poste->upload();
+
+            $analyzer = new Analyzer();
+
+            $a = array("neg => ", "new =>  ", "pos =>  ","compound =>  ");
+            $b = $analyzer->getSentiment($poste->getDescription());
+
+            $output_text =  array_combine($a,$b);
+
+
+            $json = json_encode( $output_text);
+            $poste->setDescAnalys($json );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($poste);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('postes_back_index');
+        }
+
+        return $this->render('postes/backnew.html.twig', [
+            'poste' => $poste,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/back/{idPost}", name="postes_back_show", methods={"GET"})
+     */
+    public function showback(Postes $poste): Response
+    {
+        return $this->render('postes/backshow.html.twig', [
+            'poste' => $poste,
+        ]);
+    }
+
+    /**
+     * @Route("/back/{idPost}/edit", name="postes_back_edit", methods={"GET","POST"})
+     */
+    public function editback(Request $request, Postes $poste): Response
+    {
+        $form = $this->createForm(PostesType::class, $poste);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('postes_back_index');
+        }
+
+        return $this->render('postes/backedit.html.twig', [
+            'poste' => $poste,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/back/{idPost}", name="postes_back_delete", methods={"POST"})
+     */
+    public function deleteback(Request $request, Postes $poste): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$poste->getIdPost(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($poste);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('postes_back_index');
+    }
+
+
+
+
+
+    /**
+     * @Route("/back/TriCat/show", name="poste_back_cat", methods={"POST"})
+     */
+    public function FindByCategorieback(EntityManagerInterface $em,Request $request): Response
+    {
+        $data=$request->get('myText');
+        $queryBuilder = $em->getRepository(Postes::class)->createQueryBuilder('E');
+        $queryBuilder->andWhere('E.categorie = :cat');
+        $queryBuilder->setParameter('cat', $data);
+        $postes = $queryBuilder->getQuery()->getResult();
+
+
+        $categories = $this->getDoctrine()
+            ->getRepository(Categorie::class)
+            ->findAll();
+
+        return $this->render('postes/backindex.html.twig', [
+            'postes' => $postes,'categories' => $categories,
+        ]);
+    }
+
+
+    /**
+     * @Route("/back", name="poste_back_search", methods={"POST"})
+     */
+    public function searchback(PostesRepository $posteRepo, Request $request)
+    {
+        $data=$request->get('mots');
+        $postes = $posteRepo->search($data);
+        $categories = $this->getDoctrine()
+            ->getRepository(Categorie::class)
+            ->findAll();
+
+        return $this->render('postes/backindex.html.twig', [
+            'postes' => $postes, 'categories' => $categories,
+        ]);
+    }
+
+
+
+
+
+
+
+    /**
+     * @Route("/back/tri/triTrending", name="postes_back_triTrending")
+     */
+    public function backTriTrending(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            'SELECT E FROM App\Entity\Postes E 
+            ORDER BY E.likes DESC'
+        );
+
+        $postes = $query->getResult();
+
+
+        $categories = $this->getDoctrine()
+            ->getRepository(Categorie::class)
+            ->findAll();
+
+        return $this->render('postes/backindex.html.twig', [
+            'postes' => $postes,'categories' => $categories,
+        ]);
+
+    }
+
+
+
+    /**
+     * @Route("/back/tri/triDate", name="postes_back_triDate")
+     */
+    public function backTriDate(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            'SELECT P FROM App\Entity\Postes P
+            ORDER BY P.postDate DESC'
+        );
+
+        $postes = $query->getResult();
+
+
+        $categories = $this->getDoctrine()
+            ->getRepository(Categorie::class)
+            ->findAll();
+
+        return $this->render('postes/backindex.html.twig', [
+            'postes' => $postes,'categories' => $categories,
+        ]);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
