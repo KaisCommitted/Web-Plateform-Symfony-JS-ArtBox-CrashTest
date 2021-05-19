139e62c51279c3285c67186599055e9821a8044b
louayjeddou <48138944+0Stain@users.noreply.github.com>
2021-05-19 18:38:57 +0100

    ItTimeForLoginAndRegisterThenk

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,22 +12,18 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils)
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-      if ($this->getUser()) {
-            return $this->redirectToRoute('user');
-         }
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-//        var_dump( $lastUsername);
-//        die();
-
-        return $this->render('security/login.html.twig',
-            ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
