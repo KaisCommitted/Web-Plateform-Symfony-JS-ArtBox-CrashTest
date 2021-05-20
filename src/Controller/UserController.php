c8fd047a87a68819786c51729a14d9723205ba2a
louayjeddou <48138944+0Stain@users.noreply.github.com>
2021-05-20 02:49:57 +0100

    Json User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,13 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Feedback;
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\FeedbackRepository;
+use App\Repository\UserRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Constraints\Json;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 /**
  * @Route("/user")
  */
@@ -27,6 +40,34 @@ class UserController extends AbstractController
             'users' => $users,
         ]);
     }
+
+
+    /**
+     * @Route("/displayUser", name="display_user")
+     */
+
+    public function getUser(UserRepository $UserRepository, SerializerInterface $serializerInterface)
+    {
+        $F = $UserRepository->findAll();
+        $serializer = new Serializer(
+            array(
+                new DateTimeNormalizer(array('datetime_format' => 'Y-m-d')),
+                new ObjectNormalizer()
+            )
+        );
+
+
+
+
+
+        $json = $serializer->normalize($F , 'json', [AbstractNormalizer::ATTRIBUTES => ['id_user','nom','prenom','username','mail','date_naissance','pwd_user','ref_admin','id_label','image','roles']]);
+
+
+        //$json = $serializerInterface->serialize($F, 'json' , ['groups' => 'User']);
+        return new JsonResponse($json);
+    }
+
+
 
     /**
      * @Route("/new", name="user_new", methods={"GET","POST"})
@@ -50,6 +91,44 @@ class UserController extends AbstractController
             'user' => $user,
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/addUser", name="add_user", methods={"GET","POST"})
+     */
+    public function addUserRequest (Request $request, UserRepository $userRepository)
+    {
+        $F = new User();
+        $nom = $request->query->get("nom");
+        $prenom = $request->query->get("prenom");
+        $username = $request->query->get("username");
+        $mail = $request->query->get("mail");
+        $date_naissance = $request->query->get("date_naissance");
+        $pwd_user = $request->query->get("pwd_user");
+        $ref_admin = $request->query->get("ref_admin");
+        $id_label = $request->query->get("id_label");
+        $image = $request->query->get("image");
+        $roles = $request->query->get("roles");
+        $isVerified = $request->query->get("isVerified");
+        $em = $this->getDoctrine()->getManager();
+
+        $F->setNom($nom);
+        $F->setPrenom($prenom);
+        $F->setUsername($username);
+        $F->setMail($mail);
+        $F->setDateNaissance($date_naissance);
+        $F->setPwdUser($pwd_user);
+        $F->setRefAdmin($ref_admin);
+        $F->setIdLabel($id_label);
+        $F->setImage($image);
+        $F->setRoles($roles);
+        $F->setIsVerified($isVerified);
+        $em->persist($F);
+        $em->flush();
+
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize("User added successfully");
+        return new JsonResponse($formatted);
     }
 
     /**
@@ -82,6 +161,37 @@ class UserController extends AbstractController
         ]);
     }
 
+
+    /**
+     * @Route("/updateUser", name="update_user")
+     * methods={"PUT"}
+     */
+    public function editUser(Request $request): JsonResponse
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getDoctrine()->getManager()
+            ->getRepository(User::class)
+            ->find($request->get("id_user"));
+
+        $user->setNom($request->get("nom"));
+        $user->setPrenom($request->get("prenom"));
+        $user->setUsername($request->get("username"));
+        $user->setMail($request->get("mail"));
+        $user->setDatenaissance($request->get("date_naissance"));
+        $user->setPwduser($request->get("pwd_user"));
+        $user->setRefadmin($request->get("ref_admin"));
+        $user->setIdlabel($request->get("id_label"));
+        $user->setImage($request->get("image"));
+        $user->setRoles($request->get("roles"));
+        $user->setIsVerified($request->get("isVerified"));
+        $em->persist($user);
+        $em->flush();
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize("User updated successfully");
+        return new JsonResponse($formatted);
+    }
+
+
     /**
      * @Route("/{idUser}", name="user_delete", methods={"POST"})
      */
@@ -95,6 +205,30 @@ class UserController extends AbstractController
 
         return $this->redirectToRoute('user_index');
     }
+
+
+    /**
+     * @Route("/deleteUser", name="delete_user", methods={"DELETE"})
+     */
+    public function deleteUser(Request $request): JsonResponse
+    {
+        $id_user = $request->get("id_user");
+
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository(User::class)->find($id_user);
+        if($user!=null ) {
+            $em->remove($user);
+            $em->flush();
+
+            $serialize = new Serializer([new ObjectNormalizer()]);
+            $formatted = $serialize->normalize("User successfully deleted.");
+            return new JsonResponse($formatted);
+
+        }
+        return new JsonResponse("Invalid ID.");
+
+    }
+
 
     //back-----------------------------------------------------------------------------
     /**
